b9e59291b4ccb5b302ba17f07cec9df3d65479a4
Zhendong Tang <zhendongtt@gmail.com>
2021-12-08 15:46:54 -0500

    19-008 Using useEffect with Redux

--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -1,13 +1,13 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const uiSlice = createSlice({
-  name: 'ui',
+  name: "ui",
   initialState: { cartIsVisible: false },
   reducers: {
     toggle(state) {
       state.cartIsVisible = !state.cartIsVisible;
-    }
-  }
+    },
+  },
 });
 
 export const uiActions = uiSlice.actions;
