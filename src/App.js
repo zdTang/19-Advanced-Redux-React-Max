42ad3ea22c6e8af032e9473406821a6411fa32ff
Zhendong Tang <zhendongtt@gmail.com>
2021-12-09 18:34:27 -0500

    19-010 Handling Http States & Feedback with Redux-A
    added a Notification component to display application's status
    Application's status is monitered by ui-slice
    Once any status need to be paged
    we can dispatch Notification to the ui-slice.
    In the App component, we can display this Notifition to user
    
    One issue need to be fixed: as the useEffect depends on "cart" state, when the Application is launching, the STORE will init the Cart state with initial value. this initiation will trigger the HTTP Request.  It is bad.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
-import { useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect, Fragment } from "react";
+import Notification from "./components/UI/Notification";
+import { uiActions } from "./store/ui-slice";
 
 function App() {
   const cartIsVisible = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  const notification = useSelector((state) => state.ui.notification);
   // PUT will override the old data instead appending new data
   /* ==================
    * We can monitor the update of state at any component by using useSelector
@@ -14,16 +18,62 @@ function App() {
    * option, then we can fire Http Request
    */
   useEffect(() => {
-    fetch("https://react-http-69ae2-default-rtdb.firebaseio.com/cart.json", {
-      method: "PUT",
-      body: JSON.stringify(cart),
+    // define an Async function to wrap the "fetch"
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "Sending...",
+          message: "Sending cart data!",
+        })
+      );
+      const response = await fetch(
+        "https://react-http-69ae2-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+      //
+      if (!response.ok) {
+        throw new Error("Sending cart data failed.");
+      }
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sending cart data successfully!",
+        })
+      );
+    };
+
+    // Call the defined asynchronous function
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sending cart data failed.",
+        })
+      );
     });
-  }, [cart]);
+  }, [cart, dispatch]);
+  // the "dispatch" is handled by the redux and will never change
+  // but we still need to put it here as some IDE will complain
   return (
-    <Layout>
-      {cartIsVisible && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {cartIsVisible && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
